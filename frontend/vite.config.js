ce9d981eb0dc005a951bfcdf9deba4dbe77b226e
charbell2005 <charbel.barrak901@yahoo.com>
2026-07-18 16:04:57 +0300

    Fix image paths - set base to /

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VERCEL ? '/' : '/Porfolio/',
+  base: '/',
   server: {
     port: 3000,
     proxy: {
